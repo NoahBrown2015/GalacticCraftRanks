10b08f5a3067b58c736bae3e1648c5ddca550bfe
Noah Brown <brownoah1234@gmail.com>
2021-08-12 21:15:41 -0400

    Implement Vault

--- src/main/java/me/brownie/galacticraftranks/GalacticraftRanks.java
+++ src/main/java/me/brownie/galacticraftranks/GalacticraftRanks.java
@@ -4,10 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GalaticraftRanks extends JavaPlugin {
 
+    private static Economy econ = null;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        setupEconomy();
     }
 
     @Override
@@ -15,5 +16,19 @@ public final class GalaticraftRanks extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
 }
