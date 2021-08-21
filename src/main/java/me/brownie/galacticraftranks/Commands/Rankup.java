81abc46bc988dca96a1ac976b264b80b9c9fcc2c
Noah Brown <brownoah1234@gmail.com>
2021-08-21 18:34:02 -0400

    Work on raknup command

--- src/main/java/me/brownie/galacticraftranks/Commands/Rankup.java
+++ src/main/java/me/brownie/galacticraftranks/Commands/Rankup.java
@@ -1,6 +1,7 @@
 package me.brownie.galacticraftranks.Commands;
 
 import me.brownie.galacticraftranks.GalacticraftRanks;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class Rankup {
@@ -17,7 +18,7 @@ public class Rankup {
             return;
         }
         //   Gets player's current rank
-        String group;
+        String group = "";
         for (String Group : main.getPermissions().getPlayerGroups(p)) {
             if (!main.Ranks.contains(Group)) {
                 continue;
@@ -25,15 +26,25 @@ public class Rankup {
             group = Group;
         }
         //   Get next rank from player's current
+        String nextGroup = "";
+
+        /*
+        Can't quite figure this part out :|
+         */
+
         //   Checks if player has enough money to purchase next rank
-
+        double price = main.getConfig().getDouble(nextGroup + ".cost");
+        if (!main.getEconomy().has(p,price)) {
+            main.util.sendMessage(p,"&c[GalacticraftRanks] You cannot afford to rankup!");
+            return;
+        }
         //   Removes money from player
-
+        main.getEconomy().withdrawPlayer(p,price);
         //   Removes current rank from player
-
+        main.getPermissions().playerRemoveGroup(p,group);
         //   Gives new rank to player
-
+        main.getPermissions().playerAddGroup(p,nextGroup);
         //   Announces rankup to the server
-
+        Bukkit.broadcastMessage("&6[GalacticraftRanks] " + p.getName() + " has ranked up to " + main.getConfig().getString(nextGroup + ".prefix"));
     }
 }
